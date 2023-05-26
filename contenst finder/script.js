59815c11b3613879389e9d3e7ca12da24f8144bf
Sandeep <Sandeepsaini0204@gmail.com>
2023-05-26 13:13:06 +0530

    added some more properties

--- contenst finder/script.js
+++ contenst finder/script.js
@@ -10,8 +10,13 @@ response.then((v) => {
             `<div class="card" style="width: 18rem;">
         <div class="card-body">
         <h5 class="card-title">${contenst[item].name}</h5>
+        <p>Status is:${contenst[item].status}</p>
+        <p> site is :${contenst[item].site}</p>       
         <p>Start at:${contenst[item].start_time} </p>
-        <p>End  at:${contenst[item].end_time} </p>
+        <p>End at:${contenst[item].end_time} </p>
+        <p>Duration:${contenst[item].duration} </p>
+        <p>In 24 Hours:${contenst[item].in_24_hours
+            } </p>
         <a href="${contenst[item].url}" class="btn btn-primary">Visit here</a>
         </div> 
         </div> 
